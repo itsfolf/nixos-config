7d1571609b333d4651249a6abaf68fd52ed17e90
Folf <folf@folf.me>
2026-04-09 23:44:04 +0200

    nixpkgs osmium

--- packages/osmium/default.nix
+++ packages/osmium/default.nix
@@ -1,164 +1,79 @@
 {
-  alsa-lib,
-  at-spi2-atk,
-  at-spi2-core,
-  atk,
-  autoPatchelfHook,
-  cairo,
-  cups,
-  dbus,
-  expat,
+  stdenv,
   fetchurl,
-  fontconfig,
-  freetype,
-  gdk-pixbuf,
-  glib,
-  gtk3,
   lib,
-  libX11,
-  libXScrnSaver,
-  libXcomposite,
-  libXcursor,
-  libXdamage,
-  libXext,
-  libXfixes,
-  libXi,
-  libXrandr,
-  libXrender,
-  libXtst,
-  libappindicator-gtk3,
-  libcxx,
-  libdbusmenu,
-  libdrm,
-  libgbm,
-  libglvnd,
-  libnotify,
-  libpulseaudio,
-  libunity,
-  libuuid,
-  libva,
-  libxcb,
-  libxshmfence,
   makeDesktopItem,
   makeShellWrapper,
-  nspr,
-  nss,
+  autoPatchelfHook,
+  libgcc,
+  libx11,
+  libxext,
+  libxcb,
+  libGL,
+  ffmpeg,
+  glib,
+  dbus,
   pango,
-  pipewire,
-  speechd-minimal,
-  stdenv,
-  systemdLibs,
-  wayland,
-  wrapGAppsHook3,
-  withTTS ? true,
-  enableAutoscroll ? false,
-  commandLineArgs ? "",
+  libxcomposite,
+  libxdamage,
+  libxrandr,
+  libxkbcommon,
+  libgbm,
+  nss,
+  nspr,
+  gtk3,
 }:
-stdenv.mkDerivation (finalAttrs: {
+
+stdenv.mkDerivation rec {
   pname = "osmium";
-  version = "0.0.10-alpha";
+  version = "0.0.16";
 
   src = fetchurl {
-    url = "https://updater.osmium.chat/Osmium-0.0.10-alpha-x64.tar.gz";
-    hash = "sha256-ZuV6NZptyzBXCo0YA2MfoV1OW4bzw6jcZRWf/VWafFA=";
+    url = "https://updater.osmium.chat/Osmium-${version}-alpha-x64.tar.gz";
+    hash = "sha256-dMOyZ9oPVnLt6MHeQwsMJ03wgvaKzalynwAL/PRfI28=";
   };
 
   nativeBuildInputs = [
-    alsa-lib
     autoPatchelfHook
-    cups
-    libdrm
-    libuuid
-    libXdamage
-    libX11
-    libXScrnSaver
-    libXtst
-    libxcb
-    libxshmfence
-    libgbm
-    nss
-    wrapGAppsHook3
     makeShellWrapper
   ];
 
-  dontWrapGApps = true;
-
-  libPath = lib.makeLibraryPath (
-    [
-      libcxx
-      systemdLibs
-      libpulseaudio
-      libdrm
-      libgbm
-      stdenv.cc.cc
-      alsa-lib
-      atk
-      at-spi2-atk
-      at-spi2-core
-      cairo
-      cups
-      dbus
-      expat
-      fontconfig
-      freetype
-      gdk-pixbuf
-      glib
-      gtk3
-      libglvnd
-      libnotify
-      libX11
-      libXcomposite
-      libunity
-      libuuid
-      libva
-      libXcursor
-      libXdamage
-      libXext
-      libXfixes
-      libXi
-      libXrandr
-      libXrender
-      libXtst
-      nspr
-      libxcb
-      pango
-      pipewire
-      libXScrnSaver
-      libappindicator-gtk3
-      libdbusmenu
-      wayland
-    ]
-    ++ lib.optionals withTTS [ speechd-minimal ]
-  );
+  buildInputs = [
+    libgcc
+    libx11
+    libxext
+    libxcb
+    ffmpeg
+    glib
+    dbus
+    pango
+    libxcomposite
+    libxdamage
+    libxrandr
+    libxkbcommon
+    libgbm
+    nss
+    nspr
+    gtk3
+  ];
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/{bin,opt/osmium,share/pixmaps,share/icons/hicolor/256x256/apps}
-    mv * $out/opt/osmium
+    mkdir -p $out/{bin,opt,share/{pixmaps,icons/hicolor/256x256/apps}}
 
-    chmod +x $out/opt/osmium/osmium
-    patchelf --set-interpreter ${stdenv.cc.bintools.dynamicLinker} \
-        $out/opt/osmium/osmium
+    mv * $out/opt/
+    chmod +x $out/opt/osmium
 
-    wrapProgramShell $out/opt/osmium/osmium \
-        "''${gappsWrapperArgs[@]}" \
-        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
-        ${lib.strings.optionalString withTTS ''
-          --run 'if [[ "''${NIXOS_SPEECH:-default}" != "False" ]]; then NIXOS_SPEECH=True; else unset NIXOS_SPEECH; fi' \
-          --add-flags "\''${NIXOS_SPEECH:+--enable-speech-dispatcher}" \
-        ''} \
-        ${lib.strings.optionalString enableAutoscroll "--add-flags \"--enable-blink-features=MiddleClickAutoscroll\""} \
-        --prefix XDG_DATA_DIRS : "${gtk3}/share/gsettings-schemas/${gtk3.name}/" \
-        --prefix LD_LIBRARY_PATH : ${finalAttrs.libPath}:$out/opt/osmium \
-        --add-flags ${lib.escapeShellArg commandLineArgs}
+    ln -s $out/opt/osmium $out/bin/
 
-    ln -s $out/opt/osmium/osmium $out/bin/
-    # Without || true the install would fail on case-insensitive filesystems
-    ln -s $out/opt/osmium/osmium $out/bin/osmium || true
+    wrapProgramShell $out/opt/osmium \
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
+      --prefix XDG_DATA_DIRS : "${gtk3}/share/gsettings-schemas/${gtk3.name}/" \
+      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ libGL ]}
 
-    ln -s $out/opt/osmium/resources/assets/icons/256x256.png $out/share/pixmaps/osmium.png
-    ln -s $out/opt/osmium/resources/assets/icons/256x256.png $out/share/icons/hicolor/256x256/apps/osmium.png
+    ln -s $out/opt/resources/assets/icons/256x256.png $out/share/pixmaps/osmium.png
+    ln -s $out/opt/resources/assets/icons/256x256.png $out/share/icons/hicolor/256x256/apps/osmium.png
 
     ln -s "$desktopItem/share/applications" $out/share/
 
@@ -176,21 +91,20 @@ stdenv.mkDerivation (finalAttrs: {
       "InstantMessaging"
     ];
     mimeTypes = [ "x-scheme-handler/osmium" ];
-    startupWMClass = "osmium";
+    startupWMClass = "Osmium";
   };
 
   meta = {
-    description = "A globally distributed community messaging and voice/video platform.";
-    downloadPage = "https://osmium.chat/download";
+    description = "Globally distributed community messaging and voice/video platform";
     homepage = "https://osmium.chat/";
     license = lib.licenses.unfree;
     mainProgram = "osmium";
     maintainers = with lib.maintainers; [
-      itsfolf
+      twoneis
     ];
     platforms = [
       "x86_64-linux"
     ];
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
   };
-})
+}
